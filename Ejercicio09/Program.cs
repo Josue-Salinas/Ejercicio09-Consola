7b0e4f4c714c1640fa10ea489b9febde0f0d1f79
josue <josue@DESKTOP-1SO03Q0>
2023-08-28 16:26:26 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio09/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese 3 números diferentes:");
+            Console.WriteLine("Ingrese el primer número: ");
+            int num1, num2, num3;
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número: ");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el tercer número: ");
+            num3 = int.Parse(Console.ReadLine());
+            if ((num1 > num2) && (num1 > num3))
+            {
+                Console.WriteLine("El número mayor es {0}", num1);
+            }
+            else if ((num2 > num3) && (num2 > num1))
+            {
+                Console.WriteLine("El número mayor es {0}", num2);
+            }
+            else
+            {
+                Console.WriteLine("El número mayor es {0}", num3);
+            }
+            Console.ReadKey();
+        }
+    }
+}
